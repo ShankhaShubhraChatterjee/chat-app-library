43d7d564312eb14f7c6ade311d513b5d2812c40e
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmail.com>
2025-11-28 14:26:58 +0530

    feat(styles): add messsages for sender & receiver with suitable styles

--- src/routes/components/chat-message/index.tsx
+++ src/routes/components/chat-message/index.tsx
@@ -3,9 +3,30 @@ import { createFileRoute } from '@tanstack/react-router'
 import './index.styl'
 
 export const Route = createFileRoute('/components/chat-message/')({
-  component: RouteComponent,
+	component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div className='chat__message--text'>Chat Message</div>
+	return (
+		<div className='chat__message--container'>
+			<section className='chat__message--receiver'>
+				<span className='chat__message--receiver--msg'>Message Sent To Receiver, This Message Contains Everything</span>
+			</section>
+			<section className='chat__message--sender'>
+				<span className='chat__message--sender--msg'>Message Received By Sender, This Message Contains Nothing Better Than The Receiver's</span>
+			</section>
+			<section className='chat__message--receiver'>
+				<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+			</section>
+			<section className='chat__message--receiver'>
+				<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+			</section>
+			<section className='chat__message--sender'>
+				<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+			</section>
+			<section className='chat__message--sender'>
+				<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+			</section>
+		</div>
+	)
 }
