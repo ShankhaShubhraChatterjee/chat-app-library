1e705be64b11475eda13e4303ae3875e473ba43a
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmail.com>
2025-11-28 14:28:00 +0530

    feat: add global styles for main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen.ts";
 
+import "./main.styl";
+
 const router = createRouter({ routeTree });
 
 declare module "@tanstack/react-router" {
