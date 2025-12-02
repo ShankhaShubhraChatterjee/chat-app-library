8fe7e436c4d072c964c90f3cc884cb3554d36188
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmail.com>
2025-12-02 21:01:58 +0530

    feat: remove gradient effect

--- src/routes/components/chat-message/index.tsx
+++ src/routes/components/chat-message/index.tsx
@@ -8,25 +8,99 @@ export const Route = createFileRoute('/components/chat-message/')({
 
 function RouteComponent() {
 	return (
-		<div className='chat__message--container'>
-			<section className='chat__message--receiver'>
-				<span className='chat__message--receiver--msg'>Message Sent To Receiver, This Message Contains Everything</span>
-			</section>
-			<section className='chat__message--sender'>
-				<span className='chat__message--sender--msg'>Message Received By Sender, This Message Contains Nothing Better Than The Receiver's</span>
-			</section>
-			<section className='chat__message--receiver'>
-				<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
-			</section>
-			<section className='chat__message--receiver'>
-				<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
-			</section>
-			<section className='chat__message--sender'>
-				<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
-			</section>
-			<section className='chat__message--sender'>
-				<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
-			</section>
+		<div className='chat__message--container--background'>
+			<div className='chat__message--container'>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver, This Message Contains Everything</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender, This Message Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver, This Message Contains Everything</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender, This Message Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver, This Message Contains Everything</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender, This Message Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+				</section>
+				<section className='chat__message--receiver'>
+					<span className='chat__message--receiver--msg'>Message Sent To Receiver Contains Everything</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+				</section>
+				<section className='chat__message--sender'>
+					<span className='chat__message--sender--msg'>Message Received By Sender Contains Nothing Better Than The Receiver's</span>
+				</section>
+			</div>
 		</div>
 	)
 }
