f46db95c1a1d323b4ad8703240d20d77ad3f523d
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmail.com>
2025-12-02 21:01:11 +0530

    feat: add lato font

---
+++ src/routes/shared/footer/index.tsx
@@ -0,0 +1,14 @@
+
+
+import './index.styl'
+
+const Footer = () => {
+    const date = new Date().getFullYear()
+    return (
+        <footer className="footer">
+            Copyright &copy; {date}, S.S.Chatterjee, All Rights Reserved
+        </footer>
+    )
+}
+
+export default Footer;
