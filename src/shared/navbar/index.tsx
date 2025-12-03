3ae757ad3c49d6df687a9b2090226ea44735109c
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmail.com>
2025-12-03 14:01:33 +0530

    feat(move): move 'shared' directory under 'src' directory

---
+++ src/shared/navbar/index.tsx
@@ -0,0 +1,35 @@
+import { Link } from "@tanstack/react-router";
+import "./index.styl";
+
+const Navbar = () => {
+	return (
+		<nav className="navbar">
+			<header className="navbar__header">Fluid Library</header>
+			<ul className="navbar__list">
+				<li className="navbar__list--item">
+					<Link className="navbar__list--item--link" to="/">
+						Home
+					</Link>
+				</li>
+				<li className="navbar__list--item">
+					<Link
+						className="navbar__list--item--link"
+						to="/components/chat-message"
+					>
+						Chat Message
+					</Link>
+				</li>
+				<li className="navbar__list--item">
+					<Link
+						className="navbar__list--item--link"
+						to="/components/chat-input"
+					>
+						Chat Input
+					</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
